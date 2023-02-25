551525212a050b83cce1e16b703481c72d341780
Mogalathurthi Tarun siva sai durga <tarunssd2000@gmail.com>
2023-02-25 19:42:35 +0530

    storing selected text in variable

---
+++ Google Extension/background.js
@@ -0,0 +1,26 @@
+chrome.identity.getAuthToken({interactive: true}, function(token) {
+  if (chrome.runtime.lastError) {
+    console.log(chrome.runtime.lastError);
+  } else {
+    // Use the token to make an API request
+    console.log(token);
+  }
+});  
+
+const propertiesObject = {
+  id: "copyNotes",
+  contexts: ["selection"],
+  title: "Copy to Google Docs"
+}
+
+chrome.contextMenus.create(
+  propertiesObject
+)
+
+chrome.contextMenus.onClicked.addListener(function(selectedData) {
+  console.log(selectedData.selectionText);
+  selectedText = selectedData.selectionText;
+  if (selectedData.menuItemId === "copyNotes" && selectedData.selectionText) {
+
+  }
+})
